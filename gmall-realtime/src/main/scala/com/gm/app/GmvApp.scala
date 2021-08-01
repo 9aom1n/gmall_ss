1db07a1d1e1e625ff09a8cc7530861ec0b4b1c0b
GaoMin <gaomin12666@126.com>
2021-08-01 08:45:54 +0800

    2021-08-01 08:43

---
+++ gmall-realtime/src/main/scala/com/gm/app/GmvApp.scala
@@ -0,0 +1,12 @@
+package com.gm.app
+
+/**
+ * @Author: Gm
+ * @Date: 2021/8/1 8:45
+ */
+
+object GmvApp {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
---
+++ gmall-realtime/src/main/scala/com/gm/bean/OrderInfo.scala
@@ -0,0 +1,28 @@
+package com.gm.bean
+
+/**
+ * @Author: Gm
+ * @Date: 2021/8/1 8:44
+ */
+
+case class OrderInfo(
+                      id: String,
+                      province_id: String,
+                      consignee: String,
+                      order_comment: String,
+                      var consignee_tel: String,
+                      order_status: String,
+                      payment_way: String,
+                      user_id: String,
+                      img_url: String,
+                      total_amount: Double,
+                      expire_time: String,
+                      delivery_address: String,
+                      create_time: String,
+                      operate_time: String,
+                      tracking_no: String,
+                      parent_order_id: String,
+                      out_trade_no: String,
+                      trade_body: String,
+                      var create_date: String,
+                      var create_hour: String)
